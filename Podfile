ef5e59aec05a11e69f5376f4e2cf94e106cb3673
Azuma Sato <lifeghtcloerce5291@gmail.com>
2019-04-23 01:07:35 +0900

    update swiftgen

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Tanzaku' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Tanzaku
+  pod 'RealmSwift'
+  pod 'Sparkle'
+  pod 'RxCocoa'
+  pod 'RxSwift'
+  pod 'RxOptional'
+  pod 'BartyCrouch'
+  pod 'SwiftLint'
+  pod 'SwiftGen'
+
+end
---
+++ Tanzaku/Generated/strings.swift
@@ -0,0 +1,688 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name
+internal enum L10n {
+
+  internal enum _1UK8nQPP {
+    /// Customize Toolbar…
+    internal static let title = L10n.tr("Main", "1UK-8n-QPP.title")
+  }
+
+  internal enum _1XtHYUBw {
+    /// Tanzaku
+    internal static let title = L10n.tr("Main", "1Xt-HY-uBw.title")
+  }
+
+  internal enum _1b7L0Nxx {
+    /// Find
+    internal static let title = L10n.tr("Main", "1b7-l0-nxx.title")
+  }
+
+  internal enum _1txW0XDw {
+    /// Lower
+    internal static let title = L10n.tr("Main", "1tx-W0-xDw.title")
+  }
+
+  internal enum _2h7ERAoG {
+    /// Raise
+    internal static let title = L10n.tr("Main", "2h7-ER-AoG.title")
+  }
+
+  internal enum _2oIRnZJC {
+    /// Transformations
+    internal static let title = L10n.tr("Main", "2oI-Rn-ZJC.title")
+  }
+
+  internal enum _3INSU3Bg {
+    /// Spelling
+    internal static let title = L10n.tr("Main", "3IN-sU-3Bg.title")
+  }
+
+  internal enum _3OmEy2VK {
+    /// Use Default
+    internal static let title = L10n.tr("Main", "3Om-Ey-2VK.title")
+  }
+
+  internal enum _3rSZANoH {
+    /// Speech
+    internal static let title = L10n.tr("Main", "3rS-ZA-NoH.title")
+  }
+
+  internal enum _46PCBAYj {
+    /// Tighten
+    internal static let title = L10n.tr("Main", "46P-cB-AYj.title")
+  }
+
+  internal enum _4ENYAP0u {
+    /// Find
+    internal static let title = L10n.tr("Main", "4EN-yA-p0u.title")
+  }
+
+  internal enum _4J7DPTxa {
+    /// Enter Full Screen
+    internal static let title = L10n.tr("Main", "4J7-dP-txa.title")
+  }
+
+  internal enum _4sb4sVLi {
+    /// Quit Tanzaku
+    internal static let title = L10n.tr("Main", "4sb-4s-VLi.title")
+  }
+
+  internal enum _5QFOaP0T {
+    /// Edit
+    internal static let title = L10n.tr("Main", "5QF-Oa-p0T.title")
+  }
+
+  internal enum _5VvLzBsD {
+    /// Copy Style
+    internal static let title = L10n.tr("Main", "5Vv-lz-BsD.title")
+  }
+
+  internal enum _5kVVbQxS {
+    /// About Tanzaku
+    internal static let title = L10n.tr("Main", "5kV-Vb-QxS.title")
+  }
+
+  internal enum _6dhZSVam {
+    /// Redo
+    internal static let title = L10n.tr("Main", "6dh-zS-Vam.title")
+  }
+
+  internal enum _78YHA62v {
+    /// Correct Spelling Automatically
+    internal static let title = L10n.tr("Main", "78Y-hA-62v.title")
+  }
+
+  internal enum _8mrSmYjd {
+    /// Writing Direction
+    internal static let title = L10n.tr("Main", "8mr-sm-Yjd.title")
+  }
+
+  internal enum _9icFLObx {
+    /// Substitutions
+    internal static let title = L10n.tr("Main", "9ic-FL-obx.title")
+  }
+
+  internal enum _9yt4BNSM {
+    /// Smart Copy/Paste
+    internal static let title = L10n.tr("Main", "9yt-4B-nSM.title")
+  }
+
+  internal enum AYuSKQS6 {
+    /// Main Menu
+    internal static let title = L10n.tr("Main", "AYu-sK-qS6.title")
+  }
+
+  internal enum BOFNM1cW {
+    /// Preferences…
+    internal static let title = L10n.tr("Main", "BOF-NM-1cW.title")
+  }
+
+  internal enum BgMVeC93 {
+    /// 	Left to Right
+    internal static let title = L10n.tr("Main", "BgM-ve-c93.title")
+  }
+
+  internal enum Bw7FTI3A {
+    /// Save As…
+    internal static let title = L10n.tr("Main", "Bw7-FT-i3A.title")
+  }
+
+  internal enum DVoAGPiG {
+    /// Close
+    internal static let title = L10n.tr("Main", "DVo-aG-piG.title")
+  }
+
+  internal enum Dv1IoYv7 {
+    /// Spelling and Grammar
+    internal static let title = L10n.tr("Main", "Dv1-io-Yv7.title")
+  }
+
+  internal enum F2SFzNVQ {
+    /// Help
+    internal static let title = L10n.tr("Main", "F2S-fz-NVQ.title")
+  }
+
+  internal enum FKESmKum {
+    /// Tanzaku Help
+    internal static let title = L10n.tr("Main", "FKE-Sm-Kum.title")
+  }
+
+  internal enum FalI4PZk {
+    /// Text
+    internal static let title = L10n.tr("Main", "Fal-I4-PZk.title")
+  }
+
+  internal enum FeMD8WVr {
+    /// Substitutions
+    internal static let title = L10n.tr("Main", "FeM-D8-WVr.title")
+  }
+
+  internal enum Gb9OmE27 {
+    /// Bold
+    internal static let title = L10n.tr("Main", "GB9-OM-e27.title")
+  }
+
+  internal enum GEOIwCKr {
+    /// Format
+    internal static let title = L10n.tr("Main", "GEO-Iw-cKr.title")
+  }
+
+  internal enum GUaEOCwY {
+    /// Use Default
+    internal static let title = L10n.tr("Main", "GUa-eO-cwY.title")
+  }
+
+  internal enum Gi51SRQB {
+    /// Font
+    internal static let title = L10n.tr("Main", "Gi5-1S-RQB.title")
+  }
+
+  internal enum H1bSiO9J {
+    /// Writing Direction
+    internal static let title = L10n.tr("Main", "H1b-Si-o9J.title")
+  }
+
+  internal enum H8h7bM4v {
+    /// View
+    internal static let title = L10n.tr("Main", "H8h-7b-M4v.title")
+  }
+
+  internal enum HfqGkNfa {
+    /// Text Replacement
+    internal static let title = L10n.tr("Main", "HFQ-gK-NFA.title")
+  }
+
+  internal enum HFoCyZxI {
+    /// Show Spelling and Grammar
+    internal static let title = L10n.tr("Main", "HFo-cy-zxI.title")
+  }
+
+  internal enum HyVFhRgO {
+    /// View
+    internal static let title = L10n.tr("Main", "HyV-fh-RgO.title")
+  }
+
+  internal enum I0SGh46l {
+    /// Subscript
+    internal static let title = L10n.tr("Main", "I0S-gh-46l.title")
+  }
+
+  internal enum IAoSYFd9 {
+    /// Open…
+    internal static let title = L10n.tr("Main", "IAo-SY-fd9.title")
+  }
+
+  internal enum IQvIBILA {
+    /// Window
+    internal static let title = L10n.tr("Main", "IQv-IB-iLA.title")
+  }
+
+  internal enum J5U5wG23 {
+    /// Justify
+    internal static let title = L10n.tr("Main", "J5U-5w-g23.title")
+  }
+
+  internal enum J7yLMQPV {
+    /// Use None
+    internal static let title = L10n.tr("Main", "J7y-lM-qPV.title")
+  }
+
+  internal enum KaWFt85H {
+    /// Revert to Saved
+    internal static let title = L10n.tr("Main", "KaW-ft-85H.title")
+  }
+
+  internal enum Kd2MpPUS {
+    /// Show All
+    internal static let title = L10n.tr("Main", "Kd2-mp-pUS.title")
+  }
+
+  internal enum Le2Ar0Xj {
+    /// Bring All to Front
+    internal static let title = L10n.tr("Main", "LE2-aR-0XJ.title")
+  }
+
+  internal enum LvmKoFvi {
+    /// Paste Ruler
+    internal static let title = L10n.tr("Main", "LVM-kO-fVI.title")
+  }
+
+  internal enum LbhJ2QVU {
+    /// 	Left to Right
+    internal static let title = L10n.tr("Main", "Lbh-J2-qVU.title")
+  }
+
+  internal enum MkVPrPK5 {
+    /// Copy Ruler
+    internal static let title = L10n.tr("Main", "MkV-Pr-PK5.title")
+  }
+
+  internal enum NMoOmNkz {
+    /// Services
+    internal static let title = L10n.tr("Main", "NMo-om-nkz.title")
+  }
+
+  internal enum NopCj93Q {
+    /// 	Default
+    internal static let title = L10n.tr("Main", "Nop-cj-93Q.title")
+  }
+
+  internal enum OY7WFPoV {
+    /// Minimize
+    internal static let title = L10n.tr("Main", "OY7-WF-poV.title")
+  }
+
+  internal enum OaQX3Vso {
+    /// Baseline
+    internal static let title = L10n.tr("Main", "OaQ-X3-Vso.title")
+  }
+
+  internal enum OlwNPBQN {
+    /// Hide Tanzaku
+    internal static let title = L10n.tr("Main", "Olw-nP-bQN.title")
+  }
+
+  internal enum OwMMhQMV {
+    /// Find Previous
+    internal static let title = L10n.tr("Main", "OwM-mh-QMV.title")
+  }
+
+  internal enum OyzDyDGm {
+    /// Stop Speaking
+    internal static let title = L10n.tr("Main", "Oyz-dy-DGm.title")
+  }
+
+  internal enum PtpSPVEL {
+    /// Bigger
+    internal static let title = L10n.tr("Main", "Ptp-SP-VEL.title")
+  }
+
+  internal enum Q5e8KNDq {
+    /// Show Fonts
+    internal static let title = L10n.tr("Main", "Q5e-8K-NDq.title")
+  }
+
+  internal enum R4oN2Eq4 {
+    /// Zoom
+    internal static let title = L10n.tr("Main", "R4o-n2-Eq4.title")
+  }
+
+  internal enum RB4SmHuC {
+    /// 	Right to Left
+    internal static let title = L10n.tr("Main", "RB4-Sm-HuC.title")
+  }
+
+  internal enum Rqc34CIF {
+    /// Superscript
+    internal static let title = L10n.tr("Main", "Rqc-34-cIF.title")
+  }
+
+  internal enum Ruw6mB2m {
+    /// Select All
+    internal static let title = L10n.tr("Main", "Ruw-6m-B2m.title")
+  }
+
+  internal enum S0pOCMLd {
+    /// Jump to Selection
+    internal static let title = L10n.tr("Main", "S0p-oC-mLd.title")
+  }
+
+  internal enum Td7AD5lo {
+    /// Window
+    internal static let title = L10n.tr("Main", "Td7-aD-5lo.title")
+  }
+
+  internal enum UEZBsLqG {
+    /// Capitalize
+    internal static let title = L10n.tr("Main", "UEZ-Bs-lqG.title")
+  }
+
+  internal enum VIYAgZcb {
+    /// Center
+    internal static let title = L10n.tr("Main", "VIY-Ag-zcb.title")
+  }
+
+  internal enum VdrFpXzO {
+    /// Hide Others
+    internal static let title = L10n.tr("Main", "Vdr-fp-XzO.title")
+  }
+
+  internal enum VjxXiNjq {
+    /// Italic
+    internal static let title = L10n.tr("Main", "Vjx-xi-njq.title")
+  }
+
+  internal enum W486f4Dl {
+    /// Edit
+    internal static let title = L10n.tr("Main", "W48-6f-4Dl.title")
+  }
+
+  internal enum WrgCdK1S {
+    /// Underline
+    internal static let title = L10n.tr("Main", "WRG-CD-K1S.title")
+  }
+
+  internal enum WasJATGl {
+    /// New
+    internal static let title = L10n.tr("Main", "Was-JA-tGl.title")
+  }
+
+  internal enum WeT3VZwk {
+    /// Paste and Match Style
+    internal static let title = L10n.tr("Main", "WeT-3V-zwk.title")
+  }
+
+  internal enum Xz5N4O0W {
+    /// Find…
+    internal static let title = L10n.tr("Main", "Xz5-n4-O0W.title")
+  }
+
+  internal enum YEyJHTfz {
+    /// Find and Replace…
+    internal static let title = L10n.tr("Main", "YEy-JH-Tfz.title")
+  }
+
+  internal enum YGsJ5SAR {
+    /// 	Default
+    internal static let title = L10n.tr("Main", "YGs-j5-SAR.title")
+  }
+
+  internal enum YnkF8CLZ {
+    /// Start Speaking
+    internal static let title = L10n.tr("Main", "Ynk-f8-cLZ.title")
+  }
+
+  internal enum ZM16QYy1 {
+    /// Align Left
+    internal static let title = L10n.tr("Main", "ZM1-6Q-yy1.title")
+  }
+
+  internal enum ZvOGkQUH {
+    /// Paragraph
+    internal static let title = L10n.tr("Main", "ZvO-Gk-QUH.title")
+  }
+
+  internal enum ATl1uJFS {
+    /// Print…
+    internal static let title = L10n.tr("Main", "aTl-1u-JFS.title")
+  }
+
+  internal enum AUFD15bR {
+    /// Window
+    internal static let title = L10n.tr("Main", "aUF-d1-5bR.title")
+  }
+
+  internal enum AXaAMJaq {
+    /// Font
+    internal static let title = L10n.tr("Main", "aXa-aM-Jaq.title")
+  }
+
+  internal enum AgtUL0e3 {
+    /// Use Default
+    internal static let title = L10n.tr("Main", "agt-UL-0e3.title")
+  }
+
+  internal enum BgnCTCEk {
+    /// Show Colors
+    internal static let title = L10n.tr("Main", "bgn-CT-cEk.title")
+  }
+
+  internal enum BibUjVzu {
+    /// File
+    internal static let title = L10n.tr("Main", "bib-Uj-vzu.title")
+  }
+
+  internal enum BuJUgPKt {
+    /// Use Selection for Find
+    internal static let title = L10n.tr("Main", "buJ-ug-pKt.title")
+  }
+
+  internal enum C8aY6VQd {
+    /// Transformations
+    internal static let title = L10n.tr("Main", "c8a-y6-VQd.title")
+  }
+
+  internal enum CDBIKHbR {
+    /// Use None
+    internal static let title = L10n.tr("Main", "cDB-IK-hbR.title")
+  }
+
+  internal enum CqvFjIhA {
+    /// Selection
+    internal static let title = L10n.tr("Main", "cqv-fj-IhA.title")
+  }
+
+  internal enum CwLP1Jid {
+    /// Smart Links
+    internal static let title = L10n.tr("Main", "cwL-P1-jid.title")
+  }
+
+  internal enum D9MCDAMd {
+    /// Make Lower Case
+    internal static let title = L10n.tr("Main", "d9M-CD-aMd.title")
+  }
+
+  internal enum D9cMeL2H {
+    /// Text
+    internal static let title = L10n.tr("Main", "d9c-me-L2H.title")
+  }
+
+  internal enum DMsCIMzQ {
+    /// File
+    internal static let title = L10n.tr("Main", "dMs-cI-mzQ.title")
+  }
+
+  internal enum DRJ4nYzg {
+    /// Undo
+    internal static let title = L10n.tr("Main", "dRJ-4n-Yzg.title")
+  }
+
+  internal enum GVAU4SdL {
+    /// Paste
+    internal static let title = L10n.tr("Main", "gVA-U4-sdL.title")
+  }
+
+  internal enum HQb2vFYv {
+    /// Smart Quotes
+    internal static let title = L10n.tr("Main", "hQb-2v-fYv.title")
+  }
+
+  internal enum Hz2CUCR7 {
+    /// Check Document Now
+    internal static let title = L10n.tr("Main", "hz2-CU-CR7.title")
+  }
+
+  internal enum Hz9B4Xy5 {
+    /// Services
+    internal static let title = L10n.tr("Main", "hz9-B4-Xy5.title")
+  }
+
+  internal enum I1dErQST {
+    /// Smaller
+    internal static let title = L10n.tr("Main", "i1d-Er-qST.title")
+  }
+
+  internal enum IjkEBDga {
+    /// Baseline
+    internal static let title = L10n.tr("Main", "ijk-EB-dga.title")
+  }
+
+  internal enum JbqR6Vk2 {
+    /// Kern
+    internal static let title = L10n.tr("Main", "jBQ-r6-VK2.title")
+  }
+
+  internal enum JFqTB4Kx {
+    /// 	Right to Left
+    internal static let title = L10n.tr("Main", "jFq-tB-4Kx.title")
+  }
+
+  internal enum JxTCUNIS {
+    /// Format
+    internal static let title = L10n.tr("Main", "jxT-CU-nIS.title")
+  }
+
+  internal enum KIPVfHaE {
+    /// Show Sidebar
+    internal static let title = L10n.tr("Main", "kIP-vf-haE.title")
+  }
+
+  internal enum MK62p4JG {
+    /// Check Grammar With Spelling
+    internal static let title = L10n.tr("Main", "mK6-2p-4JG.title")
+  }
+
+  internal enum O6eR0MWq {
+    /// Ligatures
+    internal static let title = L10n.tr("Main", "o6e-r0-MWq.title")
+  }
+
+  internal enum OasOcFiZ {
+    /// Open Recent
+    internal static let title = L10n.tr("Main", "oas-Oc-fiZ.title")
+  }
+
+  internal enum OgcRXTC1 {
+    /// Loosen
+    internal static let title = L10n.tr("Main", "ogc-rX-tC1.title")
+  }
+
+  internal enum Pa3QIU2k {
+    /// Delete
+    internal static let title = L10n.tr("Main", "pa3-QI-u2k.title")
+  }
+
+  internal enum Pxx59PXV {
+    /// Save…
+    internal static let title = L10n.tr("Main", "pxx-59-PXV.title")
+  }
+
+  internal enum Q09FTSye {
+    /// Find Next
+    internal static let title = L10n.tr("Main", "q09-fT-Sye.title")
+  }
+
+  internal enum QISW8SiK {
+    /// Page Setup…
+    internal static let title = L10n.tr("Main", "qIS-W8-SiK.title")
+  }
+
+  internal enum RbDRhWIN {
+    /// Check Spelling While Typing
+    internal static let title = L10n.tr("Main", "rbD-Rh-wIN.title")
+  }
+
+  internal enum RgMF4Ycn {
+    /// Smart Dashes
+    internal static let title = L10n.tr("Main", "rgM-f4-ycn.title")
+  }
+
+  internal enum SnWS8Cw5 {
+    /// Show Toolbar
+    internal static let title = L10n.tr("Main", "snW-S8-Cw5.title")
+  }
+
+  internal enum TRrPd1PS {
+    /// Data Detectors
+    internal static let title = L10n.tr("Main", "tRr-pd-1PS.title")
+  }
+
+  internal enum TXIMrWws {
+    /// Open Recent
+    internal static let title = L10n.tr("Main", "tXI-mr-wws.title")
+  }
+
+  internal enum TlDOaOAM {
+    /// Kern
+    internal static let title = L10n.tr("Main", "tlD-Oa-oAM.title")
+  }
+
+  internal enum UQyDDJDr {
+    /// Tanzaku
+    internal static let title = L10n.tr("Main", "uQy-DD-JDr.title")
+  }
+
+  internal enum URlIYUnG {
+    /// Cut
+    internal static let title = L10n.tr("Main", "uRl-iY-unG.title")
+  }
+
+  internal enum VKCJMMkH {
+    /// Paste Style
+    internal static let title = L10n.tr("Main", "vKC-jM-MkH.title")
+  }
+
+  internal enum VLm3IIUL {
+    /// Show Ruler
+    internal static let title = L10n.tr("Main", "vLm-3I-IUL.title")
+  }
+
+  internal enum VNYRzJ42 {
+    /// Clear Menu
+    internal static let title = L10n.tr("Main", "vNY-rz-j42.title")
+  }
+
+  internal enum VmV6d7jI {
+    /// Make Upper Case
+    internal static let title = L10n.tr("Main", "vmV-6d-7jI.title")
+  }
+
+  internal enum W0mVySC9 {
+    /// Ligatures
+    internal static let title = L10n.tr("Main", "w0m-vy-SC9.title")
+  }
+
+  internal enum Wb2VDLq4 {
+    /// Align Right
+    internal static let title = L10n.tr("Main", "wb2-vD-lq4.title")
+  }
+
+  internal enum Wpr3qMcd {
+    /// Help
+    internal static let title = L10n.tr("Main", "wpr-3q-Mcd.title")
+  }
+
+  internal enum X3vGGIWU {
+    /// Copy
+    internal static let title = L10n.tr("Main", "x3v-GG-iWU.title")
+  }
+
+  internal enum XQD1fW4t {
+    /// Use All
+    internal static let title = L10n.tr("Main", "xQD-1f-W4t.title")
+  }
+
+  internal enum XrEMZJX0 {
+    /// Speech
+    internal static let title = L10n.tr("Main", "xrE-MZ-jX0.title")
+  }
+
+  internal enum Z6FFW3nz {
+    /// Show Substitutions
+    internal static let title = L10n.tr("Main", "z6F-FW-3nz.title")
+  }
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg...) -> String {
+    // swiftlint:disable:next nslocalizedstring_key
+    let format = NSLocalizedString(key, tableName: table, bundle: Bundle(for: BundleToken.self), comment: "")
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+private final class BundleToken {}
