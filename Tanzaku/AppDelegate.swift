db5c1bf4f20a27a1b96ee9390aecd75db6482a69
Azuma Sato <lifeghtcloerce5291@gmail.com>
2019-04-22 23:26:35 +0900

    add xcodeproj

---
+++ Tanzaku/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tanzaku
+//
+//  Created by Azuma Sato on 2019/04/22.
+//  Copyright © 2019 Azuma Sato. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tanzaku/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tanzaku
+//
+//  Created by Azuma Sato on 2019/04/22.
+//  Copyright © 2019 Azuma Sato. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
